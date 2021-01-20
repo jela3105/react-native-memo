207f5eccc2b20e71e22f4cb0af3ccbd5492b83ae
jela3105 <jela3105@gmail.com>
2021-01-19 22:45:13 -0600

    Add styles in items of Table

--- App.js
+++ App.js
@@ -9,13 +9,26 @@ export default function App() {
       key: 0,
       image: "https://image.flaticon.com/icons/png/512/919/919826.png",
     },
+    {
+      key: 1,
+      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
+    },
+    {
+      key: 2,
+      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
+    },
+    {
+      key: 3,
+      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
+    },
   ]);
 
   const [initialModalVisibility, setInitialModalVisibility] = useState(false);
+  const numColumns = 4;
 
   return (
     <View style={styles.container}>
-      <Table cardsList={cardsList} />
+      <Table cardsList={cardsList} numColumns={numColumns} />
       <StatusBar style="auto" />
       <Modal visibility={initialModalVisibility}>
         <Text>Open up App.js to start working on your app!</Text>
@@ -27,6 +40,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 50,
     backgroundColor: "#111111",
   },
   text: {
--- components/Table.js
+++ components/Table.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { StyleSheet, View, Text, Button, FlatList } from "react-native";
+import { StyleSheet, View, Text, Button, FlatList, Image } from "react-native";
 
-export default ({ cardsList }) => {
+export default ({ cardsList, numColumns }) => {
   console.log(cardsList);
   return (
     <FlatList
       data={cardsList}
       renderItem={({ item }) => (
         <View style={styles.itemStyle}>
-          <Text style={styles.title}>{item.image}</Text>
+          <Image style={styles.photo} source={{ uri: item.image }} />
         </View>
       )}
-      keyExtractor={(item) => item}
+      keyExtractor={(item, index) => index.toString()}
+      numColumns={numColumns}
     />
   );
 };
@@ -21,8 +22,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
     alignItems: "center",
     justifyContent: "center",
-    height: 100,
+    height: 80,
+    width: 80,
     flex: 1,
+    margin: 8,
+    borderRadius: 10,
   },
   title: { color: "#000000" },
+  photo: {
+    height: 80,
+    width: 80,
+  },
 });
