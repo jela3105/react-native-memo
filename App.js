ce8d973a871358d506a40cd620bb8efbcea79696
jela3105 <jela3105@gmail.com>
2021-01-20 17:09:42 -0600

    Find couple of cards working

--- App.js
+++ App.js
@@ -4,22 +4,46 @@ import { StyleSheet, Text, View } from "react-native";
 import { Modal, Cards } from "./components";
 import cardsFile from "./assets/cards";
 
+let selectedCards = new Array();
+
 export default function App() {
   const [modalVisibility, setModalVisibility] = useState(false);
   const [cards, setCards] = useState(cardsFile.sort(() => Math.random() - 0.5));
   const numColumns = 4;
 
   const handleSelectedCard = (item) => {
-    console.log(cards);
     const modifiedCards = cards.map((x) => {
       if (x.id == item.id) {
         x.show = "true";
-        console.log(`el modificado es ${x}`);
+        selectedCards.push(item);
       }
       return x;
     });
-    console.log(modifiedCards);
+
     setCards(modifiedCards);
+    validateCards();
+  };
+
+  const validateCards = () => {
+    if (selectedCards.length == 2) {
+      if (selectedCards[0].image === selectedCards[1].image) {
+        console.log("son iguales bandaaa aaaahhh");
+        selectedCards = [];
+      } else {
+        setTimeout(() => {
+          setCards(
+            cards.map((x) => {
+              if (x.id == selectedCards[0].id || x.id == selectedCards[1].id) {
+                x.show = "false";
+              }
+              return x;
+            })
+          );
+          console.log("no son iguales :c");
+          selectedCards = [];
+        }, 900);
+      }
+    }
   };
 
   return (
