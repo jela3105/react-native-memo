a0665258cece8525c8185b175f92df670a48d989
jela3105 <jela3105@gmail.com>
2021-01-20 01:03:17 -0600

    Show images read from json

--- App.js
+++ App.js
@@ -2,147 +2,30 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Modal, Table } from "./components";
+import cardsFile from "./assets/cards";
 
 export default function App() {
-  const [cardsList, setCardsList] = useState([
-    {
-      key: 0,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 1,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 2,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 3,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 4,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 5,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 6,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 7,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 0,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 1,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 2,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 3,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 4,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 5,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 6,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 7,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 0,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 1,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 2,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 3,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 4,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 5,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 6,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 7,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 0,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 1,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 2,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-    {
-      key: 3,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    },
-  ]);
-
   const [initialModalVisibility, setInitialModalVisibility] = useState(false);
   const [cards, setCards] = useState([]);
 
   const shortCards = (cards) => {
-    console.log(cards);
-    return {
-      key: 3,
-      image: "https://image.flaticon.com/icons/png/512/919/919826.png",
-    };
+    return [
+      {
+        key: 3,
+        image: "https://image.flaticon.com/icons/png/512/919/919826.png",
+      },
+    ];
   };
 
   useEffect(() => {
-    fetch("./assets/cards.json")
-      .then((response) => response.json)
-      .then((cards) => {
-        setCards(shortCards(cards));
-      });
+    setCards(cardsFile);
   }, []);
 
   const numColumns = 4;
 
   return (
     <View style={styles.container}>
-      <Table cardsList={cardsList} numColumns={numColumns} />
+      <Table cardsList={cards} numColumns={numColumns} />
       <StatusBar style="auto" />
       <Modal visibility={initialModalVisibility}>
         <Text>Open up App.js to start working on your app!</Text>
--- components/Table.js
+++ components/Table.js
@@ -13,12 +13,20 @@ const WIDTH = Dimensions.get("window").width;
 const HEIGHT = Dimensions.get("window").height;
 
 export default ({ cardsList, numColumns }) => {
+  cardsList.map((x) => console.log(typeof x.key));
   return (
     <FlatList
       data={cardsList}
       renderItem={({ item }) => (
         <View style={styles.itemStyle}>
-          <Image style={styles.photo} source={{ uri: item.image }} />
+          <Image
+            style={styles.photo}
+            source={
+              item.show == "true"
+                ? { uri: item.image }
+                : require("../assets/hola-mundo.jpg")
+            }
+          />
         </View>
       )}
       keyExtractor={(item, index) => index.toString()}
@@ -40,7 +48,8 @@ const styles = StyleSheet.create({
   },
   title: { color: "#000000" },
   photo: {
-    width: 80,
-    height: 80,
+    width: 70,
+    height: 70,
   },
 });
+//{ uri: item.image }
