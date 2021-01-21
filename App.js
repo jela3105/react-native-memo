2687019787da5ca20a37f0ae5ce8f4e37e44ea54
jela3105 <jela3105@gmail.com>
2021-01-20 18:46:19 -0600

    Validate select card is already open

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ToastAndroid,
+  Platform,
+  AlertIOS,
+} from "react-native";
 import { Modal, Cards } from "./components";
 import cardsFile from "./assets/cards";
 
@@ -12,37 +19,58 @@ export default function App() {
   const numColumns = 4;
 
   const handleSelectedCard = (item) => {
-    const modifiedCards = cards.map((x) => {
+    if (selectedCards.length != 2) {
+      flipCard(item);
+      validateCards();
+    }
+  };
+
+  const flipCard = (item) => {
+    const newCardList = cards.map((x) => {
       if (x.id == item.id) {
-        x.show = "true";
-        selectedCards.push(item);
+        if (x.show == "false") {
+          x.show = "true";
+          selectedCards.push(item);
+        } else {
+          showMessage("Elige otra carta");
+        }
       }
       return x;
     });
-
-    setCards(modifiedCards);
-    validateCards();
+    setCards(newCardList);
   };
 
   const validateCards = () => {
     if (selectedCards.length == 2) {
       if (selectedCards[0].image === selectedCards[1].image) {
-        console.log("son iguales bandaaa aaaahhh");
+        showMessage("Felicidades, son iguales");
         selectedCards = [];
       } else {
-        setTimeout(() => {
-          setCards(
-            cards.map((x) => {
-              if (x.id == selectedCards[0].id || x.id == selectedCards[1].id) {
-                x.show = "false";
-              }
-              return x;
-            })
-          );
-          console.log("no son iguales :c");
-          selectedCards = [];
-        }, 900);
+        hideCards();
       }
+    }
+  };
+
+  const hideCards = () => {
+    setTimeout(() => {
+      setCards(
+        cards.map((x) => {
+          if (x.id == selectedCards[0].id || x.id == selectedCards[1].id) {
+            x.show = "false";
+          }
+          return x;
+        })
+      );
+      console.log("no son iguales :c");
+      selectedCards = [];
+    }, 800);
+  };
+
+  const showMessage = (message) => {
+    if (Platform.OS === "android") {
+      ToastAndroid.show(message, ToastAndroid.SHORT);
+    } else {
+      AlertIOS.alert(message);
     }
   };
 
